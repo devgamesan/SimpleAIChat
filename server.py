210c4fc97d377e82eb9af453da40f6daf139ef4c
devgamesan <devgamesan@gmail.com>
2025-05-24 14:28:22 +0900

    Add server code for test

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 
-PORT = 8000
+PORT = 9999
 
 class RequestHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=os.path.dirname(__file__), **kwargs)
+
+    def send_head(self):
+        path = self.translate_path(self.path)
+        if os.path.isdir(path):
+            return super().send_head()
+        
+        ctype = self.guess_type(path)
+        if ctype.startswith('text/'):
+            ctype += '; charset=utf-8'
+        return super().send_head()
 
 def run_server():
     server_address = ('', PORT)
